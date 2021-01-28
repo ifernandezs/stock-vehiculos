30c132592f6ca6d60577b440e141e0bd709fd99d
igfernan <ignacio.fernandez@piperlab.es>
2021-01-28 22:59:04 +0100

    creacion del repositorio

---
+++ src/main/java/com/meep/poc/stockvehiculos/StockVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.meep.poc.stockvehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockVehiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meep/poc/stockvehiculos/StockVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meep.poc.stockvehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
